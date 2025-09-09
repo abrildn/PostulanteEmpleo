000393d70206acf5647d7823a73634f4db20b9e8
Abril <abrudomingueznoya@gmail.com>
2025-09-09 13:53:55 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Postulante
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la cantidad de preguntas que se realizaron");
+            string linea = Console.ReadLine();
+            double preg = double.Parse(linea);
+
+            Console.WriteLine("Ingrese la cantidad que contestó correctamente");
+            linea = Console.ReadLine();
+            double correc = double.Parse(linea);
+
+            double porc = correc / 100 * preg;
+            double noventa = 90.0 / 100 * preg;
+            double sete = 75.0 / 100 * preg;
+            double cinc = 50.0 / 100 * preg;
+            
+            if (porc >= noventa)
+            {
+                Console.WriteLine("Nivel Máximo");
+            }
+            else
+            {
+                if (porc >= sete && porc < noventa)
+                {
+                    Console.WriteLine("Nivel Medio");
+                }
+                else
+                {
+                    if (porc >= cinc && porc < sete)
+                    {
+                        Console.WriteLine("Nivel Regular");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Fuera de nivel");
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
